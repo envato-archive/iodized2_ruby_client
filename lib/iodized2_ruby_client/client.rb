28e89b71053f77604dc92f61f073cfdf043a9710
Lucas Caton <lucascaton@gmail.com>
2019-04-16 17:48:34 +1000

    [WIP] Implement Iodized2RubyClient::Client

--- lib/iodized2_ruby_client/client.rb
+++ lib/iodized2_ruby_client/client.rb
@@ -1,21 +1,42 @@
+require 'iodized2_ruby_client/ws_client'
+
 module Iodized2RubyClient
   class Client
+    attr_reader :features
+
     def initialize(url)
+      @url = url
+      @features = []
+
+      @ws_client = Iodized2RubyClient::WSClient.new(@url) do |message|
+        handle_message(message)
+      end
     end
 
-    def features
-      [
-        {
-          id: 1,
-          name: "name",
-          active: false
-        },
-        {
-          id: 2,
-          name: "name2",
-          active: true
-        }
-      ]
+    def handle_message(message)
+      result = JSON.parse(message)
+
+      key = result.keys.first
+      send(key, result[key])
+    end
+
+    private
+
+    def sync(features)
+      @features = features
+    end
+
+    def create(feature)
+      # TODO: implement-me!
+      # @features << feature
+    end
+
+    def update(feature)
+      # TODO: implement-me!
+    end
+
+    def delete(feature)
+      # TODO: implement-me!
     end
   end
 end
